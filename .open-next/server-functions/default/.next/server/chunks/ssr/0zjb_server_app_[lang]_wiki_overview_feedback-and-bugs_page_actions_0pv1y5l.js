a2657e473f0ad80334792912f174f1dabefd1eab
PhuPham <tranphupham2909@gmail.com>
2026-05-04 02:16:35 +0700

    chore: remove .open-next from tracking

--- .open-next/server-functions/default/.next/server/chunks/ssr/0zjb_server_app_[lang]_wiki_overview_feedback-and-bugs_page_actions_0pv1y5l.js
+++
@@ -1,3 +0,0 @@
-module.exports=[19826,(a,b,c)=>{}];
-
-//# sourceMappingURL=0zjb_server_app_%5Blang%5D_wiki_overview_feedback-and-bugs_page_actions_0pv1y5l.js.map
--- .open-next/server-functions/default/.next/server/chunks/ssr/_next-internal_server_app_[lang]_download_page_actions_032ql-~.js
+++
@@ -1,3 +0,0 @@
-module.exports=[83070,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_%5Blang%5D_download_page_actions_032ql-~.js.map
